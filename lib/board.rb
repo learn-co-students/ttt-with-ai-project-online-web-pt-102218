e98b13a23ef49b415cd278589600cee829045ba1
grant smith <grantsmith1256@gmail.com>
2018-12-28 02:05:30 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,55 +1,36 @@
+require 'pry'
 class Board
   
   attr_accessor :cells
   
-  def initialize
-    self.cells = Array.new(9, " ")
+  def initialize(cells = Array.new(9," "))
+    @cells = cells
   end
   
   def reset!
-    self.cells = Array.new(9, " ")
+    @cells.clear
+    @cells = Array.new(9," ")
   end
   
   def display
-    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
     puts "-----------"
-    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
     puts "-----------"
-    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+    puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
   
   def position(input)
-    @cells[input.to_i-1]
+    "#{@cells[input.to_i - 1]}"
   end
   
   def full?
-    if @cells.include?(" ")
-      return false
-    else
-      return true
-    end
+    @cells.all? {|position| position != " "}
   end
   
   def turn_count
-    @cells.count("X") + @cells.count("O")
+    @cells.count("X") + @cell.count("O")
   end
   
-  def taken?(input)
-    if @cells[input.to_i - 1] == " "
-      return false
-    else
-      true
-    end
-  end
-  
-  def valid_move?(input)
-    if @cells[input.to_i - 1] == " " && input.to_i.between?(1,9)
-      return true
-    end
-  end
-  
-  def update(position, player)
-    self.cells[position.to_i-1] = player.token
-  end
   
 end
